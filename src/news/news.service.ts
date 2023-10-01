07fdd59665658524825a0fe745aea2af019743d8
fahust <kevin.dellova@hotmail.fr>
2023-10-01 19:24:44 +0200

    feat: create article and add notification to investors of project

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { NewsEntity } from './news.entity';
+import { ProjectService } from 'src/project/project.service';
+import { NotificationService } from 'src/notification/notification.service';
 
 @Injectable()
 export class NewsService {
   constructor(
     @InjectRepository(NewsEntity)
     private newsRepository: Repository<NewsEntity>,
+    private projectService: ProjectService,
+    private notificationService: NotificationService,
   ) {}
 
   async get(id: number): Promise<NewsEntity> {
@@ -15,6 +19,16 @@ export class NewsService {
       .createQueryBuilder('news')
       .where('news.id = :id', { id })
       .getOne();
+  }
+
+  async create(news: NewsEntity, projectId: number): Promise<NewsEntity> {
+    const project = await this.projectService.get(projectId);
+    const newsSaved = await this.save({
+      ...news,
+      project,
+    });
+    await this.notificationService.newArticle(project);
+    return newsSaved;
   }
 
   async save(news: NewsEntity): Promise<NewsEntity> {
